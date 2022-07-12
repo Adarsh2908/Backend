7e8fe31cba2d2486bf65afa6b5b64e02f4628ef2
Adarsh2908 <44540660+Adarsh2908@users.noreply.github.com>
2022-07-12 16:04:20 +0530

    Update dbDetails.js
    Added Dummy text in place of Real Links

--- vocab/config/dbDetails.js
+++ vocab/config/dbDetails.js
@@ -1,3 +1,3 @@
 module.exports = {
-    connectionURL : "mongodb+srv://adarshsimar:adarsh998877665544simar%40@cluster0.jocid.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    connectionURL : "The Connection URL removed for Security Purposes"
 }
